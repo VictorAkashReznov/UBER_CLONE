6b7633d1f00f62233a6c47298a9b49252467614f
VictorAkashReznov <akashkushwaha5956@gmail.com>
2025-01-10 00:25:30 +0530

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const { connect } = require("mongoose");
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectToDb();
 
 app.get("/", (req, res) => {
   res.send(" hello world ");
 });
+app.use("/users", userRoutes);
+
 module.exports = app;
